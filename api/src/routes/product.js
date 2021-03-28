ebe3c5e96a1b460927c16269d583c55d01386b0b
matiasracedo <matiasracedo@gmail.com>
2021-03-28 01:14:39 -0300

    functioning shopping cart

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -5,10 +5,20 @@ const server = express.Router();
 server.get("/", (req, res) => {
     connection.query('SELECT * FROM `product`', (error, results) => {
         // error will be an Error if one occurred during the query
-        if (error) res.json({ error: err })
+        if (error) res.json({ err: error })
         // results will contain the results of the query
         return res.json(results);
       })
+})
+
+server.get("/:id", (req, res) => {
+  let { id } = req.params;
+  connection.query('SELECT * FROM `product` WHERE `id` = ?', [id], (error, result) => {
+    // error will be an Error if one occurred during the query
+    if (error) res.json({ err: error })
+    // results will contain the result of the query
+    return res.json(result);
+  })
 })
 
 module.exports = server;
